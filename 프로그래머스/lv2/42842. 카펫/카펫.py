d1e932de3b441fe3901082027a418e0eaa8bac3b
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-04-14 01:14:04 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,11 +1,2 @@
 def solution(brown, yellow):
-    answer = []
-    for i in range(1,yellow+1):
-        if yellow % i == 0 and (i + 2) * 2 + (yellow/i) * 2 == brown :
-            if i >= yellow//i:
-                answer.extend([i+2,yellow//i+2])
-                break
-            else:
-                answer.extend([yellow//i+2,i+2])
-                break
-    return answer
+    return next([yellow//i+2, i+2] for i in range(1, int(yellow**0.5)+1) if yellow % i == 0 and (i + yellow//i) * 2 + 4 == brown)
