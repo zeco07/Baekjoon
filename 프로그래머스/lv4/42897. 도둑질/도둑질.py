5b9c426a2882355cf0988fb5ad78fa465583f678
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-04-14 21:24:49 +0900

    [level 4] Title: 도둑질, Time: 578.61 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.py
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -1,17 +1,17 @@
 def solution(money):
     n = len(money)
-    dp1 = [0] * n # 첫 번째 집을 털었을 때의 최대 값
-    dp2 = [0] * n # 첫 번째 집을 안 털었을 때의 최대 값
-
-    # 첫 번째 집을 털었을 때
+    dp1 = [0] * len(money)
+    dp2 = [0] * len(money)
+    
+    #첫 번째 집 부터 터는 경우
     dp1[0] = money[0]
-    dp1[1] = max(money[0], money[1])
-    for i in range(2, n-1):
-        dp1[i] = max(dp1[i-1], dp1[i-2]+money[i])
-
-    # 첫 번째 집을 안 털었을 때
+    dp1[1] = max(money[0],money[1])
+    for i in range(2, n-1):         # 첫 번째 집을 털면 마지막 집은 못털어서 n-1 까지
+        dp1[i] = max(dp1[i-1],dp1[i-2]+money[i])
+    
+    #두 번째 집 부터 터는 경우
     dp2[1] = money[1]
-    for i in range(2, n):
-        dp2[i] = max(dp2[i-1], dp2[i-2]+money[i])
-
-    return max(dp1[-2], dp2[-1])
+    for i in range(2,n):            # 마지막 집 털수 있어서 n 까지
+        dp2[i] = max(dp2[i-1],dp2[i-2]+money[i])
+    
+    return max(max(dp1),max(dp2))
