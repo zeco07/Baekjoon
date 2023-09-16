09c92fd77abae30f235c693797779bb75dd73977
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-09-16 20:40:58 +0900

    Update 방 배정.py

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -9,5 +9,8 @@ for i in range(len(li)):
         if j != 0 and j <= K:
             count += 1
         elif j != 0 and j > K:
-            count += j//K+1
+            if j%K == 0:
+                count += j//K
+            else:
+                count += j//K+1
 print(count)
