3426121c0df4e06a12c0487adb9453a8631fb88a
Dylan_Han <zeco07@naver.com>
2023-09-16 20:38:43 +0900

    Revert "Delete 백준/Bronze/13300. 방 배정 directory"
    This reverts commit 94c36c2099568883c6b9a3a525f297d0d12542cb.

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+li = [[0,0,0,0,0,0], [0,0,0,0,0,0]]
+for i in range(N):
+    S, Y = map(int, input().split())
+    li[S][Y-1]+=1
+count = 0
+for i in range(len(li)):
+    for j in li[i]:
+        if j != 0 and j <= K:
+            count += 1
+        elif j != 0 and j > K:
+            count += j//K+1
+print(count)
