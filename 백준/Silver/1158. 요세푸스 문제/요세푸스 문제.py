0d2a8aedc1c37f2614d91440d7d746d582509575
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-09-19 16:31:59 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,13 @@
+n, k = map(int,input().split())
+
+people = [_ for _ in range(1,n+1)]
+ans = []
+idx = k-1
+while (people):
+    if idx >= len(people):
+        idx %= len(people)
+    ans.append(people.pop(idx))
+    idx += k-1
+
+formatted_list = "<" + ", ".join(map(str, ans)) + ">"
+print(formatted_list)
