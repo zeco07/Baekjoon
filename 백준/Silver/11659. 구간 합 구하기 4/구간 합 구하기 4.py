0abd23cb99cb4083b3d26556215001bc5e44270e
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-10-11 01:47:57 +0900

    Update 구간 합 구하기 4.py

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -5,15 +5,12 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 n_li = list(map(int, input().split()))
+pfs = [0]
+sum = 0
+for i in n_li:
+    sum += i
+    pfs.append(sum)
 
-dp = [0] * n
-dp[0] = n_li[0]
-for i in range(1, n):
-    dp[i] = dp[i - 1] + n_li[i]
-
-for _ in range(m):
+for i in range(m):
     i, j = map(int, input().split())
-    if i < 2:
-        print(dp[j - 1])
-    else:
-        print(dp[j - 1] - dp[i - 2])
+    print(pfs[j] - pfs[i - 1])
