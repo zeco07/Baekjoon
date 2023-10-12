1e3c3b60907a69f4c8cce6e402e12cb743624bb3
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-10-12 18:04:10 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n + 1)
+if n < 3:
+    print(1)
+
+else:
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[n])
