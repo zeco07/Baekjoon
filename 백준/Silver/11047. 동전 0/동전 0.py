79e1dce330432922fcaf92c1d5ef3e55bd6daab5
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-10-13 23:18:07 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+a = [int(input()) for _ in range(n)]
+
+cnt = 0
+for i in reversed(range(n)):
+    if k == 0:
+        break
+    cnt += k // a[i]
+    k %= a[i]
+
+print(cnt)
