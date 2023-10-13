017731fcd41eb8abc33d40fdb1bf95bddeaff5ec
Dylan_Han <69717135+zeco07@users.noreply.github.com>
2023-10-13 23:43:18 +0900

    [Silver I] Title: 회의실 배정, Time: 284 ms, Memory: 58284 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,8 +4,7 @@ input = sys.stdin.readline
 
 n = int(input())
 schdule = [list(map(int, input().split())) for _ in range(n)]
-schdule.sort()
-schdule.sort(key=lambda x: x[1])
+schdule.sort(key=lambda x: (x[1],x[0]))
 ans = [schdule[0]]
 
 for i in range(1, n):
